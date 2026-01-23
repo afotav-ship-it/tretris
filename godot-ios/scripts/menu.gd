90437141e52bd34487f78eef0d17d28db6b38c0b
Afonso Tavares <afotav@users.noreply.github.com>
2026-01-23 17:56:33 +0100

    Fix menu node lookup: defer setup and safe connects

--- godot-ios/scripts/menu.gd
+++ godot-ios/scripts/menu.gd
@@ -6,18 +6,46 @@ signal exit_pressed
 
 const Constants = preload("res://scripts/constants.gd")
 
-@onready var btn_play = $VBox/BtnMenuPlay
-@onready var btn_exit = $VBox/BtnMenuExit
-@onready var btn_normal = $VBox/ModeBox/BtnMenuNormal
-@onready var btn_advanced = $VBox/ModeBox/BtnMenuAdvanced
-@onready var btn_boulder = $VBox/ModeBox/BtnMenuBoulder
+var btn_play: Button
+var btn_exit: Button
+var btn_normal: Button
+var btn_advanced: Button
+var btn_boulder: Button
 
 func _ready() -> void:
-    btn_play.pressed.connect(_on_play_pressed)
-    btn_exit.pressed.connect(_on_exit_pressed)
-    btn_normal.pressed.connect(_on_normal_pressed)
-    btn_advanced.pressed.connect(_on_advanced_pressed)
-    btn_boulder.pressed.connect(_on_boulder_pressed)
+    # Safely find child nodes; some instancing contexts may delay child creation.
+    btn_play = get_node_or_null("VBox/BtnMenuPlay")
+    btn_exit = get_node_or_null("VBox/BtnMenuExit")
+    btn_normal = get_node_or_null("VBox/ModeBox/BtnMenuNormal")
+    btn_advanced = get_node_or_null("VBox/ModeBox/BtnMenuAdvanced")
+    btn_boulder = get_node_or_null("VBox/ModeBox/BtnMenuBoulder")
+
+    if not btn_play or not btn_exit or not btn_normal:
+        # Defer setup once the scene is fully ready
+        call_deferred("_deferred_setup")
+        return
+
+    _connect_buttons()
+
+func _deferred_setup() -> void:
+    btn_play = get_node_or_null("VBox/BtnMenuPlay")
+    btn_exit = get_node_or_null("VBox/BtnMenuExit")
+    btn_normal = get_node_or_null("VBox/ModeBox/BtnMenuNormal")
+    btn_advanced = get_node_or_null("VBox/ModeBox/BtnMenuAdvanced")
+    btn_boulder = get_node_or_null("VBox/ModeBox/BtnMenuBoulder")
+    _connect_buttons()
+
+func _connect_buttons() -> void:
+    if btn_play:
+        btn_play.pressed.connect(_on_play_pressed)
+    if btn_exit:
+        btn_exit.pressed.connect(_on_exit_pressed)
+    if btn_normal:
+        btn_normal.pressed.connect(_on_normal_pressed)
+    if btn_advanced:
+        btn_advanced.pressed.connect(_on_advanced_pressed)
+    if btn_boulder:
+        btn_boulder.pressed.connect(_on_boulder_pressed)
 
 func _on_play_pressed() -> void:
     emit_signal("start_pressed")
